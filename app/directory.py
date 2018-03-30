825178d099b5def35710c24c0dd0a50028ded0de
miko <ruteckimikolaj@gmail.com>
2018-03-30 22:42:51 +0200

    incremental backup works fine in back.py file.

---
+++ app/directory.py
@@ -0,0 +1,13 @@
+class Directory():
+
+    def dir1(self):
+        self= 'r"'+input("Enter first directory")+r'"'
+        return self
+
+    def dir2():
+        y=input()
+        return y
+
+
+if __name__=="__main__":
+    directory.run(debug=True)
